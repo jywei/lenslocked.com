3f117b451c51c3fbb2f71bf3e7c3c98bd4e46a69
Roy Wei <roywjy@gmail.com>
2022-05-16 17:25:29 +1000

    Add contact

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	// fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+	fmt.Fprint(w, "To get in touch, please send and email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
 func main() {
