f8c669eed8a79eae28432da5002c14e789d265c8
Roy Wei <roywjy@gmail.com>
2022-05-17 12:00:28 +1000

    Refactor the if-else statement

--- main.go
+++ main.go
@@ -7,21 +7,31 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send and email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We can't find the page you are looking for!</h1>")
-	}
-}
+// func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html")
+// 	if r.URL.Path == "/" {
+// 		fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+// 	} else if r.URL.Path == "/contact" {
+// 		fmt.Fprint(w, "To get in touch, please send and email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+// 	} else {
+// 		w.WriteHeader(http.StatusNotFound)
+// 		fmt.Fprint(w, "<h1>We can't find the page you are looking for!</h1>")
+// 	}
+// }
 
 // func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 // 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 // }
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send and email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+}
 
 func main() {
 	// mux := &http.ServeMux{}
@@ -30,9 +40,10 @@ func main() {
 	// router.GET("/hello/:name", Hello)
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlerFunc)
-	r.HandleFunc("/contact", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":5000", r)
 
 	// http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":5000", r)
+	// http.ListenAndServe(":5000", nil)
 }
