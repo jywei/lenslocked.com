06f98b511199dc3bb4d1596fcb0b7a193b9522b7
Roy Wei <roywjy@gmail.com>
2022-05-17 12:02:03 +1000

    Apply refactor

--- main.go
+++ main.go
@@ -7,22 +7,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// func handlerFunc(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html")
-// 	if r.URL.Path == "/" {
-// 		fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
-// 	} else if r.URL.Path == "/contact" {
-// 		fmt.Fprint(w, "To get in touch, please send and email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-// 	} else {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		fmt.Fprint(w, "<h1>We can't find the page you are looking for!</h1>")
-// 	}
-// }
-
-// func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-// 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-// }
-
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
@@ -34,16 +18,8 @@ func contact(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// mux := &http.ServeMux{}
-	// mux.HandleFunc("/", handlerFunc)
-	// router := httprouter.New()
-	// router.GET("/hello/:name", Hello)
-
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":5000", r)
-
-	// http.HandleFunc("/", handlerFunc)
-	// http.ListenAndServe(":5000", nil)
 }
