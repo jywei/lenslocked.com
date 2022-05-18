6a3f94f6888d05692fb5c19a85994a5cb5fb3956
Roy Wei <roywjy@gmail.com>
2022-05-18 17:42:53 +1000

    Remove wrong line

--- main.go
+++ main.go
@@ -39,8 +39,6 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	homeView = NewView("views/home.gohtml")
-
 	var err error
 	homeTemplate, err = template.ParseFiles(
 		"views/home.gohtml",
